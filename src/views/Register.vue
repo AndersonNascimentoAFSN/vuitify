07b94c69c810e69bbe08aad3e15482e435871581
New Author Name <anderson.nascimentoafsn@gmail.com>
2021-12-30 13:53:58 -0300

    list-group-item

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,29 +1,71 @@
 <template>
-  <div class="ma-0 pa-0">
-    <v-card
-      class="mx-auto"
-      width="300"
+  <v-navigation-drawer
+    permanent
+    expand-on-hover
+  >
+    <v-list
+      nav
+      dense
     >
-      <v-list>
-        <v-list-item>
+      <v-list-item-group
+        v-model="model"
+        active-class="active-item"
+        mandatory
+        color="#0056A8"
+      >
+
+        <v-list-item link>
           <v-list-item-icon>
             <v-icon>mdi-home</v-icon>
           </v-list-item-icon>
-
           <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-head-question</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>FAQ</v-list-item-title>
         </v-list-item>
 
         <!-- start -->
         <v-list-group
           :value="false"
-          prepend-icon="mdi-account-circle"
+          prepend-icon="mdi-forum-outline"
+          active-class="active-sub-item"
+          color="#0056A8"
+          link
         >
           <template v-slot:activator>
-            <v-list-item-title>Users</v-list-item-title>
+            <v-list-item-title>PAD</v-list-item-title>
           </template>
 
           <v-list-item
-            v-for="([title, icon], i) in admins"
+            v-for="([title, icon], i) in pad"
+            :key="i"
+            link
+            color="black"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title v-text="title"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <!-- end -->
+
+        <!-- start -->
+        <v-list-group
+          :value="false"
+          prepend-icon="mdi-account-sync"
+          link
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Turnover</v-list-item-title>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in turnover"
             :key="i"
             link
           >
@@ -33,12 +75,49 @@
             <v-list-item-title v-text="title"></v-list-item-title>
           </v-list-item>
         </v-list-group>
+        <!-- end -->
+
         <!-- start -->
+        <v-list-group
+          :value="false"
+          prepend-icon="mdi-strategy"
+          link
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Acomp. Estratégico</v-list-item-title>
+          </template>
 
-      </v-list>
-    </v-card>
-  </div>
+          <v-list-item
+            v-for="([title, icon], i) in strategicMonitoring"
+            :key="i"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title v-text="title"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <!-- end -->
 
+        <v-list-item>
+          <v-list-item-icon link>
+            <v-icon>mdi-lock</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Permissões</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon link>
+            <v-icon>mdi-account-group</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Profissionais</v-list-item-title>
+        </v-list-item>
+
+      </v-list-item-group>
+    </v-list>
+
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -47,24 +126,22 @@ export default {
 
   data() {
     return {
-      items: [
-        {
-          icon: 'mdi-wifi',
-          text: 'Wifi',
-        },
-        {
-          icon: 'mdi-bluetooth',
-          text: 'Bluetooth',
-        },
-        {
-          icon: 'mdi-chart-donut',
-          text: 'Data Usage',
-        },
+      items: {
+
+      },
+      pad: [
+        ['Novo PAD', 'mdi-plus-thick'],
+        ['Painel', 'mdi-view-dashboard'],
+        ['Indicadores', 'mdi-equalizer'],
+        ['Visão Time', 'mdi-account-multiple'],
       ],
-      model: 1,
-      admins: [
-        ['Management', 'mdi-account-multiple-outline'],
-        ['Settings', 'mdi-cog-outline'],
+      turnover: [
+        ['Painel', 'mdi-view-dashboard'],
+        ['Indicadores', 'mdi-equalizer'],
+      ],
+      strategicMonitoring: [
+        ['Painel', 'mdi-view-dashboard'],
+        ['Indicadores', 'mdi-equalizer'],
       ],
     };
   },
@@ -72,17 +149,27 @@ export default {
 </script>
 
 <style scoped>
-.border {
+/* .border {
   border: 2px dashed orange;
+} */
+
+.active-item {
+  color: #0056a8;
+}
+
+.active-sub-item {
+  color: #dddddd;
 }
 
 .v-list-item--link {
-  /* display: flex; */
-  /* justify-content: center !important; */
-  padding-left: 70px;
+  background: #f5f5f5;
 }
 
-/* .c_card {
-  max-width: 400px !important;
+/* .v-list-item .v-list-item--link .theme--light {
+  background: #f5f5f5 !important;
 } */
+
+.v-list-group__items {
+  padding-left: 70px;
+}
 </style>
---
+++ src/views/TesteRegister.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="ma-0 pa-0">
+    <v-card
+      class="mx-auto"
+      width="300"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <!-- start -->
+        <v-list-group
+          :value="false"
+          prepend-icon="mdi-account-circle"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Users</v-list-item-title>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in admins"
+            :key="i"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title v-text="title"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <!-- start -->
+
+      </v-list>
+    </v-card>
+  </div>
+
+</template>
